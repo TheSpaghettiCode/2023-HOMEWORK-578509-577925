68974c86f01b923bb49faf9b62d361dcd02921fe
PiergiF <129057935+PiergiF@users.noreply.github.com>
2023-03-28 13:03:11 +0200

    Update StanzaTest.java
    inizio modifiche StanzaTest

--- mio_diadia/test/it/uniroma3/diadia/ambienti/StanzaTest.java
+++ mio_diadia/test/it/uniroma3/diadia/ambienti/StanzaTest.java
@@ -1,19 +1,43 @@
 package it.uniroma3.diadia.ambienti;
+import it.uniroma3.diadia.attrezzi.*;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class StanzaTest {
+				Stanza principale;
+				Stanza adiacente;
+				String direzioni[] = {"nord", "sud", "est", "ovest"};
+				String attrezzi[] = {"osso", "spada", "lama", "lanterna", "coltello", "pietra", "chiavi", "pozione", "scudo", "pergamena"};
+
+	@Before
+	public void setUp(){
+					principale = new Stanza("StanzaPrincipaleTest");
+	   	adiacente = new Stanza("StanzaAdiacenteTest");
+	}
+
+//factory methods
+	private Attrezzo attrezzoTest(int i){
+					Attrezzo attrezzoTest = new Attrezzo(attrezzi[i],1);
+	}
+//da completare
+	private Stanza stanzaAdiacenteStessoNome(int i){
+					Stanza stanzaStessoNome = new Stanza("");
+	}
 
 	@Test
+
+	private 
+
+
 	public void testImpostaStanze(){
-	    Stanza stanzaCentrale = new Stanza("Stanza Centrale");
+	 /* Stanza stanzaCentrale = new Stanza("Stanza Centrale");
 	    Stanza stanzaNord= new Stanza("Stanza Nord");
 	    Stanza stanzaSud= new Stanza("Stanza Sud");
 	    Stanza stanzaEst= new Stanza("Stanza Est");
 	    Stanza stanzaOvest= new Stanza("Stanza Ovest");
-	    
+	 */ 
 	    stanzaCentrale.impostaStanzaAdiacente("nord", stanzaNord);
 	    stanzaCentrale.impostaStanzaAdiacente("sud", stanzaSud);
 	    stanzaCentrale.impostaStanzaAdiacente("est", stanzaEst);
